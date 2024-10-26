7a2f961a3ca5c3862773752308cde7a1823acebe
srimant0203 <srimant.acharjya@gmail.com>
2024-10-26 11:29:51 +0530

    Update cron.ts

--- backend/src/cron/cron.ts
+++ backend/src/cron/cron.ts
@@ -1,9 +1,9 @@
-import cron from "cron";
+import { CronJob } from "cron";
 import https from "https";
 
 const URL: string = "https://mern-booking-website-x63e.onrender.com";
 
-const job = new cron.CronJob("*/14 * * * *", function () {
+const job = new CronJob("*/14 * * * *", function () {
 	https
 		.get(URL, (res) => {
 			if (res.statusCode === 200) {
